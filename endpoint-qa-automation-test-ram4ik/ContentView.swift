67b2936d0612aeddcaa1e5d40b3dce26a59b3235
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-05-21 10:15:40 +0300

    Initial Commit

---
+++ endpoint-qa-automation-test-ram4ik/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  endpoint-qa-automation-test-ram4ik
+//
+//  Created by Ramill Ibragimov on 5/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ endpoint-qa-automation-test-ram4ik/endpoint_qa_automation_test_ram4ikApp.swift
@@ -0,0 +1,17 @@
+//
+//  endpoint_qa_automation_test_ram4ikApp.swift
+//  endpoint-qa-automation-test-ram4ik
+//
+//  Created by Ramill Ibragimov on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct endpoint_qa_automation_test_ram4ikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
